982d529fd9404f8939c7675879d97c0ed4b0e989
Sumit Mukhi <sumit.universal2008@gmail.com>
2024-02-29 01:59:54 +0530

    first commit

--- src/popup/Counter/index.tsx
+++ src/popup/Counter/index.tsx
@@ -1,37 +1,39 @@
 import { useEffect, useState } from 'react'
-import { storage } from 'webextension-polyfill'
+// import { storage } from 'webextension-polyfill'
 import { getCurrentTab } from '../../helpers/tabs'
 
 export const Counter = () => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState<any | null>(null)
 
   useEffect(() => {
     const readBackgroundMessage = async () => {
       const tab = await getCurrentTab()
-      const tabId = tab.id
+      const tabURL = tab.url
+      setValue(tabURL)
 
-      if (tabId) {
-        const data = await storage.local.get(tabId.toString())
-        const currentValue = data?.[tabId] ?? 0
+      // if (tabId) {
+      //   const data = await storage.local.get(tabId.toString())
+      //   const currentValue = data?.[tabId] ?? 0
 
-        setValue(currentValue)
-      }
+      //   setValue(currentValue)
+      // }
     }
 
     readBackgroundMessage()
   }, [])
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        fontSize: '24px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      Clicks: {value}
-    </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     fontSize: '24px',
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //   }}
+    // >
+    //   Clicks: {value}
+    // </div>
+    <p> The current website url is  :  {value}</p>
   )
 }
